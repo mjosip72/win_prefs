33573b054e401b48b272c3968e605dabc30edf2b
mjosip72 <josip.mojzes12345@gmail.com>
2022-07-03 21:37:12 +0200

    arrays, formatted json

--- lib/win_prefs.dart
+++ lib/win_prefs.dart
@@ -8,8 +8,12 @@ Preferences prefs = Preferences();
 
 class Preferences {
 
+  bool formatted = false;
+
   final Map<String, dynamic> _values = {};
   String? _fileName;
+
+  // #region int, double, string, bool
 
   void putInt(String key, int value) => _values[key] = value;
   void putDouble(String key, double value) => _values[key] = value;
@@ -36,6 +40,39 @@ class Preferences {
     return value is bool ? value : defaultValue;
   }
 
+  // #endregion
+
+  // #region arrays
+
+  void putIntArray(String key, List<int> array) => _values[key] = array;
+  void putDoubleArray(String key, List<double> array) => _values[key] = array;
+  void putStringArray(String key, List<String> array) => _values[key] = array;
+  void putBoolArray(String key, List<bool> array) => _values[key] = array;
+
+  List<int> getIntArray(String key) {
+    dynamic value = _values[key];
+    return value is List<int> ? value : [];
+  }
+
+  List<double> getDoubleArray(String key) {
+    dynamic value = _values[key];
+    return value is List<double> ? value : [];
+  }
+
+  List<String> getStringArray(String key) {
+    dynamic value = _values[key];
+    return value is List<String> ? value : [];
+  }
+
+  List<bool> getBoolArray(String key) {
+    dynamic value = _values[key];
+    return value is List<bool> ? value : [];
+  }
+
+  // #endregion
+
+  // #region open, close
+
   void open(String fileName) {
 
     if(_fileName != null) close();
@@ -43,7 +80,10 @@ class Preferences {
     _fileName = fileName;
 
     File file = File(fileName);
-    if(file.existsSync()) _values.addAll(jsonDecode(file.readAsStringSync()));
+    if(!file.existsSync()) return;
+
+    String json = file.readAsStringSync();
+    _values.addAll(JsonDecoder().convert(json));
 
   }
 
@@ -52,11 +92,16 @@ class Preferences {
     if(_fileName == null) return;
 
     File file = File(_fileName!);
-    file.writeAsStringSync(jsonEncode(_values));
+
+    JsonEncoder encoder = formatted ? JsonEncoder.withIndent('\t') : JsonEncoder();
+    String json = encoder.convert(_values);
+    file.writeAsStringSync(json);
 
     _values.clear();
     _fileName = null;
 
   }
+
+  // #endregion
 
 }
